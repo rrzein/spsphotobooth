7da4aa262e9401039b06294916661da2963d9855
Ricky Zein <rrzein@gmail.com>
2013-09-12 17:07:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spsphotobooth::Application.config.secret_token = '8cd3f79449043bf7bf42d8caf7be35ceb27ef06cc438318dd24750e24c79905789e7fa5658fde006d2d8ff17bb882922952bdad7e552a2d399474bc6e85c8566'
